d1bfe9cc88254654d15447ef36c065230655bc5b
mohsin-ali08 <mosisurhio@icloud.com>
2026-05-17 03:31:19 +0500

    linear Equation & Simple calculator file added

---
+++ 2 Linear Equation.py
@@ -0,0 +1,15 @@
+
+
+# 2. Linear Equation (Basic Arithmetic)
+
+a = float(input("\nEnter value of a: "))
+b = float(input("Enter value of b: "))
+
+x = -b / a
+
+print(f"\nValue of x is:{x}\n")
+
+
+# 🔹 Explanation:
+# This program solves a simple linear equation of the form ax + b = 0. We use the formula x = -b / a.
+# User enters values of a and b, and the program calculates the value of x.
---
+++ 3. Simple calculator.py
@@ -0,0 +1,26 @@
+# Task 3 - Simple Calculator
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("\n--- Results ---")
+
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
+
+
+
+
+
+# 🔹 Explanation (Simple English):
+
+# This program works like a basic calculator.
+# It takes two numbers from the user as input.
+# Then it performs four operations:
+
+# Addition (+)
+# Subtraction (-)
+# Multiplication (*)
+# Division (/)
+

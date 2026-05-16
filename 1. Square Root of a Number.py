b4c1bb4ccef656107c3f9da219805754f97d2c3d
mohsin-ali08 <mosisurhio@icloud.com>
2026-05-17 02:50:55 +0500

    first commit

---
+++ 1. Square Root of a Number.py
@@ -0,0 +1,18 @@
+
+
+
+num = float(input("\nEnter a number: "))
+sqrt = num ** 0.5 
+print(f"Square root is: {sqrt}\n")
+
+
+
+# 🔹 Explanation:
+
+# Is program me user se number liya jata hai.
+# Phir ** 0.5 use karke uska square root nikalte hain.
+# Last me result print hota hai.
+
+# 🔹 Example Output:
+# Enter a number: 25
+# Square root is: 5.0
